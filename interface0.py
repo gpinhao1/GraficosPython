4a76e2b6bebd4d4771fe7b079ffb4abd4de5aa7e
gabrielpinhao <gabrielpinhao@id.uff.br>
2025-12-17 18:12:03 -0300

    finalizado

--- interface0.py
+++ interface0.py
@@ -11,17 +11,17 @@ class FTIRApp:
         self.root.iconbitmap(r"C:\Users\Gabriel\Desktop\uerj.ico")
 
         self.frame = tk.Frame(root, width=280, height=200, bg="Black")
-        self.frame.pack(pady=5, expand=True)
+        self.frame.pack(pady=1, expand=True)
 
-        self.status_label = tk.Label(self.frame, text="Carregue o Arquivo Primeiro", font=("Times New Roman", 13))
-        self.status_label.pack(pady=5, expand=True)
+        self.status_label = tk.Label(self.frame, text="Bem vindo \n Escolha o Arquivo", font=("Times New Roman", 13))
+        self.status_label.pack(pady=1, expand=True)
 
         self.load_button = tk.Button(root, text="Arquivo Excel", command=self.load_file,font=("Times New Roman", 13))
-        self.load_button.pack(pady=5, expand=True)
+        self.load_button.pack(pady=1, expand=True)
 
 
         self.plot_button = tk.Button(root, text="Gere o Grafico", command=self.plot_spectrum,font=("Times New Roman", 13))
-        self.plot_button.pack(pady=5, expand=True)
+        self.plot_button.pack(pady=1, expand=True)
 
         self.df = None
 
@@ -32,7 +32,7 @@ class FTIRApp:
             self.df = pd.read_excel(file_path, header=1, decimal=',')
             print("File loaded successfully!")
             print(self.df.head())
-            aviso = "Arquivo Carregado com Sucesso!"
+            aviso = "Arquivo Carregado \n com Sucesso!"
             self.status_label.config(text=aviso)
         else:
             print("Erro ao carregar.")
@@ -53,8 +53,24 @@ class FTIRApp:
         else:
             print("Carregue o Arquivo Primeiro.")
 
+    def centralizar_janela(janela, largura, altura):
+
+        # 1. Obter as dimensões da tela
+        largura_tela = janela.winfo_screenwidth()
+        altura_tela = janela.winfo_screenheight()
+
+        # 2. Calcular a posição (x e y) para centralizar
+        pos_x = int((largura_tela / 2) - (largura / 2))
+        pos_y = int((altura_tela / 2) - (altura / 2))
+
+        # 3. Aplicar a geometria com a posição calculada
+        # Formato: "LarguraxAltura+PosicaoX+PosicaoY"
+        janela.geometry(f'{largura}x{altura}+{pos_x}+{pos_y}')
+
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("300x300")
+    largura = 250
+    altura = 250
+    FTIRApp.centralizar_janela(root, largura, altura)
     app = FTIRApp(root)
     root.mainloop()
